7eb136e5c1e1eb89f7baa26152c470d542ca80a4
jone28 <97891416+jone28@users.noreply.github.com>
2022-01-18 15:34:38 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { MongoClient, ObjectId } = require("mongodb");
 
 //const url = "mongodb://localhost:27017";
- const url = "mongodb+srv://admin:abc123@cluster0.uzlkl.mongodb.net";
+ const url = "mongodb+srv://admin:abcd123@cluster0.uzlkl.mongodb.net";
 
 const dbName = "ocean_bancodados_18_01_2022";
 
